230c7e3ea716372f4c44bb9d48637e0e31a2cd40
OnajiteChristabel <christabelonajite2005@gmail.com>
2023-06-06 12:52:29 -0700

    t

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+from flask import Flask, render_template
 import tkinter as tk
 import random
 from threading import Lock
 from pygame import mixer
 from PIL import Image, ImageTk
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/game')
+def game():
+    pass
 
 def play_song():
     mixer.init()
@@ -19,15 +30,15 @@ class Tetris():
     FIELD_WIDTH = 10
     SCORE_PER_ELIMINATED_LINES = (0, 40, 100, 300, 1200)
     TETROMINOS = [
-        [(0, 0), (0, 1), (1, 0), (1,1)], # O
-        [(0, 0), (0, 1), (1, 1), (2,1)], # L
-        [(0, 1), (1, 1), (2, 1), (2,0)], # J 
-        [(0, 1), (1, 0), (1, 1), (2,0)], # Z
-        [(0, 1), (1, 0), (1, 1), (2,1)], # T
-        [(0, 0), (1, 0), (1, 1), (2,1)], # S
-        [(0, 1), (1, 1), (2, 1), (3,1)], # I
+        [(0, 0), (0, 1), (1, 0), (1, 1)],  # O
+        [(0, 0), (0, 1), (1, 1), (2, 1)],  # L
+        [(0, 1), (1, 1), (2, 1), (2, 0)],  # J
+        [(0, 1), (1, 0), (1, 1), (2, 0)],  # Z
+        [(0, 1), (1, 0), (1, 1), (2, 1)],  # T
+        [(0, 0), (1, 0), (1, 1), (2, 1)],  # S
+        [(0, 1), (1, 1), (2, 1), (3, 1)],  # I
     ]
-    
+
     def __init__(self):
         self.field = [[0 for c in range(Tetris.FIELD_WIDTH)] for r in range(Tetris.FIELD_HEIGHT)]
         self.score = 0
@@ -39,117 +50,144 @@ class Tetris():
 
     def reset_tetromino(self):
         self.tetromino = random.choice(Tetris.TETROMINOS)[:]
-        self.tetromino_color = random.randint(1, len(COLORS)-1)
-        self.tetromino_offset = [-2, Tetris.FIELD_WIDTH//2]
+        self.tetromino_color = random.randint(1, len(COLORS) - 1)
+        self.tetromino_offset = [-2, Tetris.FIELD_WIDTH // 2]
         self.game_over = any(not self.is_cell_free(r, c) for (r, c) in self.get_tetromino_coords())
-    
+
     def get_tetromino_coords(self):
-        return [(r+self.tetromino_offset[0], c + self.tetromino_offset[1]) for (r, c) in self.tetromino]
+        return [(r + self.tetromino_offset[0], c + self.tetromino_offset[1]) for (r, c) in self.tetromino]
 
     def apply_tetromino(self):
         for (r, c) in self.get_tetromino_coords():
             self.field[r][c] = self.tetromino_color
 
         new_field = [row for row in self.field if any(tile == 0 for tile in row)]
-        lines_eliminated = len(self.field)-len(new_field)
+        lines_eliminated = len(self.field) - len(new_field)
         self.total_lines_eliminated += lines_eliminated
-        self.field = [[0]*Tetris.FIELD_WIDTH for x in range(lines_eliminated)] + new_field
+        self.field = [[0] * Tetris.FIELD_WIDTH for x in range(lines_eliminated)] + new_field
         self.score += Tetris.SCORE_PER_ELIMINATED_LINES[lines_eliminated] * (self.level + 1)
         self.level = self.total_lines_eliminated // 10
         self.reset_tetromino()
 
     def get_color(self, r, c):
         return self.tetromino_color if (r, c) in self.get_tetromino_coords() else self.field[r][c]
-    
+
     def is_cell_free(self, r, c):
-        return r >= 0 and c >= 0 and r < Tetris.FIELD_HEIGHT and c < Tetris.FIELD_WIDTH and self.field[r][c] == 0
+        return r < Tetris.FIELD_HEIGHT and 0 <= c < Tetris.FIELD_WIDTH and (r < 0 or self.field[r][c] == 0)
 
-    def move_tetromino(self, dr, dc):
+    def move(self, dr, dc):
         with self.move_lock:
-            for (r, c) in self.get_tetromino_coords():
-                if not self.is_cell_free(r+dr, c+dc):
-                    return False
-            self.tetromino_offset[0] += dr
-            self.tetromino_offset[1] += dc
-            return True
+            if self.game_over:
+                return
 
-    def rotate_tetromino(self):
+            if all(self.is_cell_free(r + dr, c + dc) for (r, c) in self.get_tetromino_coords()):
+                self.tetromino_offset = [self.tetromino_offset[0] + dr, self.tetromino_offset[1] + dc]
+            elif dr == 1 and dc == 0:
+                self.game_over = any(r < 0 for (r, c) in self.get_tetromino_coords())
+                if not self.game_over:
+                    self.apply_tetromino()
+
+    def rotate(self):
         with self.move_lock:
-            original_tetromino = self.tetromino
-            self.tetromino = [(c, -r) for (r, c) in self.tetromino[::-1]]
-            if not all(self.is_cell_free(r+self.tetromino_offset[0], c+self.tetromino_offset[1]) for (r, c) in self.get_tetromino_coords()):
-                self.tetromino = original_tetromino
+            if self.game_over:
+                self.__init__()
+                return
+
+            ys = [r for (r, c) in self.tetromino]
+            xs = [c for (r, c) in self.tetromino]
+            size = max(max(ys) - min(ys), max(xs) - min(xs))
+            rotated_tetromino = [(c, size - r) for (r, c) in self.tetromino]
+            wallkick_offset = self.tetromino_offset[:]
+            tetromino_coord = [(r + wallkick_offset[0], c + wallkick_offset[1]) for (r, c) in rotated_tetromino]
+            min_x = min(c for r, c in tetromino_coord)
+            max_x = max(c for r, c in tetromino_coord)
+            max_y = max(r for r, c in tetromino_coord)
+            wallkick_offset[1] -= min(0, min_x)
+            wallkick_offset[1] += min(0, Tetris.FIELD_WIDTH - (1 + max_x))
+            wallkick_offset[0] += min(0, Tetris.FIELD_HEIGHT - (1 + max_y))
+
+            tetromino_coord = [(r + wallkick_offset[0], c + wallkick_offset[1]) for (r, c) in rotated_tetromino]
+            if all(self.is_cell_free(r, c) for (r, c) in tetromino_coord):
+                self.tetromino, self.tetromino_offset = rotated_tetromino, wallkick_offset
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.master = master
-        self.master.geometry('500x600')
-        self.master.bind('<KeyPress>', self.on_key_press)
-        self.master.bind('<KeyRelease>', self.on_key_release)
-        self.pack()
+        self.tetris = Tetris()
+        self.pack(fill=tk.BOTH, expand=True)
         self.create_widgets()
 
     def create_widgets(self):
-        self.canvas = tk.Canvas(self, width=400, height=400, bg='black')
-        self.canvas.pack(side="top")
+        image = Image.open("static/img2.jpeg")
+        image = image.resize((self.master.winfo_screenwidth(), self.master.winfo_screenheight()))
+        photo = ImageTk.PhotoImage(image)
 
-        self.score_label = tk.Label(self, text="Score: 0", font=("Helvetica", 16))
-        self.score_label.pack(side="top")
+        image_label = tk.Label(self, image=photo)
+        image_label.image = photo
+        image_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.level_label = tk.Label(self, text="Level: 0", font=("Helvetica", 16))
-        self.level_label.pack(side="top")
+        self.play_button = tk.Button(self, text="Play", font=("TimesNewRoman", 40), command=self.start_game)
+        self.play_button.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
 
-    def on_key_press(self, event):
-        if event.keysym == 'Up':
-            game.rotate_tetromino()
-        elif event.keysym == 'Down':
-            self.after(50, self.move_tetromino_down)
-        elif event.keysym == 'Left':
-            self.move_tetromino_sideways(-1)
-        elif event.keysym == 'Right':
-            self.move_tetromino_sideways(1)
+    def start_game(self):
+        self.master.withdraw()
+        game_window = tk.Toplevel()
+        game_window.title("Tetris")
+        game_window.resizable(20, 20)
+        app = GameApplication(master=game_window)
+        play_song()
 
-    def on_key_release(self, event):
-        if event.keysym == 'Down':
-            self.after_cancel(self.after_id)
 
-    def move_tetromino_down(self):
-        if game.move_tetromino(1, 0):
-            self.redraw()
-            self.after_id = self.after(500 - 50*game.level, self.move_tetromino_down)
+class GameApplication(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.tetris = Tetris()
+        self.pack()
+        self.create_widgets()
+        self.update_clock()
+
+    def update_clock(self):
+        self.tetris.move(1, 0)
+        self.update()
+        if self.tetris.game_over:
+            stop_song()
         else:
-            game.apply_tetromino()
-            if game.game_over:
-                self.game_over()
+            self.master.after(int(1000 * (0.8 ** self.tetris.level)), self.update_clock)
 
-    def move_tetromino_sideways(self, direction):
-        game.move_tetromino(0, direction)
-        self.redraw()
+    def create_widgets(self):
+        PIECE_SIZE = 30
+        self.canvas = tk.Canvas(self, height=PIECE_SIZE * self.tetris.FIELD_HEIGHT,
+                                width=PIECE_SIZE * self.tetris.FIELD_WIDTH, bg="black", bd=0)
+        self.canvas.bind('<Left>', lambda _: (self.tetris.move(0, -1), self.update()))
+        self.canvas.bind('<Right>', lambda _: (self.tetris.move(0, 1), self.update()))
+        self.canvas.bind('<Down>', lambda _: (self.tetris.move(1, 0), self.update()))
+        self.canvas.bind('<Up>', lambda _: (self.tetris.rotate(), self.update()))
+        self.canvas.focus_set()
+        self.rectangles = [
+            self.canvas.create_rectangle(c * PIECE_SIZE, r * PIECE_SIZE, (c + 1) * PIECE_SIZE, (r + 1) * PIECE_SIZE,
+                                         outline="", fill="")
+            for r in range(self.tetris.FIELD_HEIGHT) for c in range(self.tetris.FIELD_WIDTH)
+        ]
+        self.canvas.pack(side="left")
 
-    def redraw(self):
-        self.canvas.delete("all")
-        for r in range(Tetris.FIELD_HEIGHT):
-            for c in range(Tetris.FIELD_WIDTH):
-                color = COLORS[game.get_color(r, c)]
-                x1, y1 = c * 20, r * 20
-                x2, y2 = x1 + 20, y1 + 20
-                self.canvas.create_rectangle(x1, y1, x2, y2, fill=color)
-        
-        self.score_label["text"] = f"Score: {game.score}"
-        self.level_label["text"] = f"Level: {game.level}"
+        self.title_msg = tk.Label(self, text="Tetris", font=("TimesNewRoman", 40), fg="black", bg="grey")
+        self.title_msg.pack(side="top", pady=10)
+        self.status_msg = tk.Label(self, anchor='w', width=11, font=("Courier", 24))
+        self.status_msg.pack(side="top")
+        self.game_over_msg = tk.Label(self, anchor='w', width=11, font=("Courier", 24), fg='red')
+        self.game_over_msg.pack(side="top")
 
-    def game_over(self):
-        self.canvas.delete("all")
-        x1, y1 = 50, 200
-        x2, y2 = x1 + 300, y1 + 100
-        self.canvas.create_rectangle(x1, y1, x2, y2, fill='white')
-        self.canvas.create_text(200, 250, text="Game Over", font=("Helvetica", 30))
+    def update(self):
+        for i, _id in enumerate(self.rectangles):
+            color_num = self.tetris.get_color(i // self.tetris.FIELD_WIDTH, i % self.tetris.FIELD_WIDTH)
+            self.canvas.itemconfig(_id, fill=COLORS[color_num])
 
-def main():
-    root = tk.Tk()
-    app = Application(master=root)
-    app.mainloop()
+        self.status_msg['text'] = "Score: {}\nLevel: {}".format(self.tetris.score, self.tetris.level)
+        self.game_over_msg['text'] = "GAME OVER.\nPress UP\nto reset" if self.tetris.game_over else ""
 
-if __name__ == '__main__':
-    main()
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
+root.mainloop()
